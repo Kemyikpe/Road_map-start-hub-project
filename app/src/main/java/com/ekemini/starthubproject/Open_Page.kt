d8d54cac0db648e1e284b46d732a95c69e9ca4d8
KemyIkpe <kemyikpe@gmail.com>
2023-02-19 19:15:26 +0100

    Road map project

---
+++ app/src/main/java/com/ekemini/starthubproject/Open_Page.kt
@@ -0,0 +1,11 @@
+package com.ekemini.starthubproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Open_Page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_open_page)
+    }
+}
---
+++ app/src/main/java/com/ekemini/starthubproject/SignUp_Page.kt
@@ -0,0 +1,16 @@
+package com.ekemini.starthubproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp_Page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_page)
+
+
+
+    }
+
+    }
+
